2f20b5f214f120014e4b66c4500670c347d08d48
filipe-mateus <filipemaateus1@gmail.com>
2022-10-06 23:08:45 -0300

    att

--- FP-flask/website/admin/rotas.py
+++ FP-flask/website/admin/rotas.py
@@ -1,18 +1,28 @@
-from flask import render_template, session, request, url_for, flash, redirect, flash
+from flask import render_template, session, request, url_for, redirect, flash
 from website  import app, db, bcrypt
 from .models import User
 
 import  os
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    email = request.form.get("usuario")
+    senha = request.form.get("senha")
+
+    if request.method == "POST":
+        user = User.query.filter_by(email=email).first()
+        if user and bcrypt.check_password_hash(user.senha, senha):
+            nome = user.nome
+            session['email'] = email
+            flash(f'login feito com sucesso','success')
+            return redirect(url_for('dash_al', nome=nome))
+
     return render_template('admin/login.html')
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
-
-
 
     senha = request.form.get("password")
     confirmar_senha = request.form.get("Confirmapassword")
@@ -47,8 +57,13 @@ def cadastro():
         db.session.commit()
 
 
-        flash(f'Obrigado por registrasse')
+        #flash(f'Obrigado por registrasse')
         return redirect(url_for('login'))
 
 
     return render_template('admin/cadastro.html')
+
+@app.route('/dash_al')
+def dash_al():
+
+    return render_template('admin/page-dashboard-al.html')
